14d98beae237ecb1f19c8014e6968d02d07340d3
olive1618 <emily.shouppe@storicard.com>
2020-05-24 18:18:14 -0500

    Save to file

--- main.go
+++ main.go
@@ -4,19 +4,23 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func CreateFileIfNotExists(sail_or_board string, fileloc string) bool {
-	var sail_header = []string{"Brand", "Name", "Year", "Cost", "Size (Sq Meter)", "Num Battens", "Has Sailbag", "New", "Notes"}
-	var board_header = []string{"Brand", "Name", "Year", "Cost", "Length (cm)", "Liters", "Inflatable", "New", "Notes"}
+var sail_fileloc string = "./data/saved_sails.csv"
+var board_fileloc string = "./data/saved_boards.csv"
+var sail_header = []string{"Brand", "Name", "Year", "Cost", "Size (Sq Meter)", "Num Battens", "Has Sailbag", "New", "Notes"}
+var board_header = []string{"Brand", "Name", "Year", "Cost", "Length (cm)", "Liters", "Inflatable", "New", "Notes"}
 
+func CreateFileIfNotExists(sail_or_board string, fileloc string) bool {
 	success := true
 
 	if _, err := os.Stat(fileloc); os.IsNotExist(err) {
 		file, err := os.Create(fileloc)
 		if err != nil {
 			fmt.Printf("Could not create file at %s", fileloc)
+			fmt.Println(err)
 			success = false
 		}
 		defer file.Close()
@@ -29,12 +33,14 @@ func CreateFileIfNotExists(sail_or_board string, fileloc string) bool {
 				err := writer.Write(sail_header)
 				if err != nil {
 					fmt.Printf("Could not write header to sail file at %s", fileloc)
+					fmt.Println(err)
 					success = false
 				}
 			} else {
 				err := writer.Write(board_header)
 				if err != nil {
 					fmt.Printf("Could not write header to board file at %s", fileloc)
+					fmt.Println(err)
 					success = false
 				}
 			}
@@ -101,12 +107,37 @@ func AddSail() Sail {
 func (s Sail) showSail() {
 	fmt.Println("Let's review the new sail\n")
 	fmt.Printf("%d %s by %s\n", s.year, s.name, s.brand)
-	fmt.Printf("Cost: %.2f\n", s.cost)
+	fmt.Printf("Cost: $%.2f\n", s.cost)
 	fmt.Printf("Size: %.1f sq meter\n", s.size_m2)
 	fmt.Printf("Num battens: %d\n", s.num_battens)
 	fmt.Printf("Sailbag: %t\n", s.has_sailbag)
 	fmt.Printf("New: %t\n", s.new)
 	fmt.Println(s.notes)
+}
+
+func saveSailToFile(s Sail) {
+	newline := []string{
+		s.brand, s.name, strconv.Itoa(s.year), strconv.FormatFloat(float64(s.cost), 'f', 2, 32),
+		strconv.FormatFloat(float64(s.size_m2), 'f', 2, 32), strconv.Itoa(s.num_battens),
+		strconv.FormatBool(s.has_sailbag), strconv.FormatBool(s.new), s.notes}
+
+	file, err := os.OpenFile(sail_fileloc, os.O_APPEND|os.O_WRONLY, 0644)
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("Could not open sail file in append mode")
+		fmt.Println(err)
+		return
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	write_err := writer.Write(newline)
+
+	if write_err != nil {
+		fmt.Println("Could not append line to sail file")
+		fmt.Println(write_err)
+	}
 }
 
 type Board struct {
@@ -167,17 +198,39 @@ func AddBoard() Board {
 func (b Board) showBoard() {
 	fmt.Println("Let's review the new board\n")
 	fmt.Printf("%d %s by %s\n", b.year, b.name, b.brand)
-	fmt.Printf("Cost: %.2f\n", b.cost)
+	fmt.Printf("Cost: $%.2f\n", b.cost)
 	fmt.Printf("Length: %d cm.  Liters: %d\n", b.length_cm, b.liters)
 	fmt.Printf("Inflatable: %t\n", b.inflatable)
 	fmt.Printf("New: %t\n", b.new)
 	fmt.Println(b.notes)
 }
 
-func main() {
-	sail_fileloc := "./data/saved_sails.csv"
-	board_fileloc := "./data/saved_boards.csv"
+func saveBoardToFile(b Board) {
+	newline := []string{
+		b.brand, b.name, strconv.Itoa(b.year), strconv.FormatFloat(float64(b.cost), 'f', 2, 32),
+		strconv.Itoa(b.length_cm), strconv.Itoa(b.liters),
+		strconv.FormatBool(b.inflatable), strconv.FormatBool(b.new), b.notes}
 
+	file, err := os.OpenFile(board_fileloc, os.O_APPEND|os.O_WRONLY, 0644)
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("Could not open board file in append mode")
+		fmt.Println(err)
+		return
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	write_err := writer.Write(newline)
+
+	if write_err != nil {
+		fmt.Println("Could not append line to board file")
+		fmt.Println(write_err)
+	}
+}
+
+func main() {
 	sail_file_success := CreateFileIfNotExists("sail", sail_fileloc)
 	board_file_success := CreateFileIfNotExists("board", board_fileloc)
 
@@ -196,10 +249,12 @@ func main() {
 			fmt.Println("Ok let's add a sail")
 			a_sail := AddSail()
 			a_sail.showSail()
+			saveSailToFile(a_sail)
 		} else if selection_lower == "board" {
 			fmt.Println("Ok let's add a board")
 			a_board := AddBoard()
 			a_board.showBoard()
+			saveBoardToFile(a_board)
 		} else if selection_lower == "no" {
 			add_another = false
 			fmt.Println("Ok bye")
